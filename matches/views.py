c5ea3569e986b704efe2b762697309204e9c14c8
Vishal Tyagi <viishaltyagi@gmail.com>
2019-01-23 03:26:11 -0500

    Matches GET API incl. filtering

--- matches/views.py
+++ matches/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Match
+from teams.models import Team
+from django.db.models import Q
 from .serializers import MatchSerializer
-import django_filters
+from django_filters import rest_framework as filters
 
 class MatchView(generics.RetrieveUpdateDestroyAPIView):
     lookup_field = 'pk'
@@ -17,8 +19,25 @@ class MatchCreate(generics.CreateAPIView):
     def get_queryset(self):
         return Match.objects.all()
 
+class MatchFilter(filters.FilterSet):
+    start_date = filters.DateFilter(field_name="date_time", lookup_expr='gte')
+    end_date = filters.DateFilter(field_name="date_time", lookup_expr='lte')
+    date_range = filters.DateRangeFilter(field_name='date_time')
+    class Meta:
+        model = Match
+        fields = ('first_team', 'second_team', 'start_date', 'end_date', 'date_range',)
+
+
 class MatchGet(generics.ListAPIView):
+    lookup_field = 'pk'
+    queryset = Match.objects.all()
     serializer_class = MatchSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = MatchFilter
 
     def get_queryset(self):
-        return Match.objects.all()
+        qs = Match.objects.all()
+        team_id = self.request.query_params.get('team', None)
+        if team_id is not None:
+            qs = qs.filter(Q(first_team=team_id) | Q(second_team=team_id))
+        return qs
--- sportshi/settings.py
+++ sportshi/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drf_multiple_model',
+    'django_filters',
     'teams',
     'matches',
 ]
@@ -128,7 +129,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    )
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
 }
 
 STATIC_URL = '/static/'
